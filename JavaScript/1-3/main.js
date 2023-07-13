139f50236f629355e6ea7657d5be01b036edb258
Ryo-dera <nogi.ume0106@gmail.com>
2023-07-13 19:01:43 +0900

    「条件分岐・演算子」の課題提出
    論理演算子と条件を満たす記述

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,17 @@
+console.log ("a != 10")
+console.log ("b = 10>20 && b % 2 === 0")
+
+
+let x = 10;
+if (x = 10<= 20){
+    console.log("成功です")
+} else{
+    console.log("失敗です")
+}
+
+let y = 0;
+if (y = y % 2 === 0){
+    console.log("偶数です")
+} else{
+    console.log("奇数です")
+}
