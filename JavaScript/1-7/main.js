ac11728a05756fe39711460be52061bca61a4ead
Ryo-dera <nogi.ume0106@gmail.com>
2023-08-03 18:57:32 +0900

    「クラスとインスタンス」の課題提出
    「Taiyaki」クラスの作成。

---
+++ JavaScript/1-7/main.js
@@ -0,0 +1,11 @@
+class Taiyaki {
+    constructor(nakami){
+        this.nakami = nakami;
+    }
+}
+let anko = new Taiyaki("あんこ");
+console.log("中身は" + anko.nakami + "です");
+let cream = new Taiyaki("クリーム");
+console.log("中身は" + cream.nakami + "です");
+let cheese = new Taiyaki("チーズ");
+console.log("中身は" + cheese.nakami + "です");
